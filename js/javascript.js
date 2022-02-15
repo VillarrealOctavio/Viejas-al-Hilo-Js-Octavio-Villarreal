da7c70be1bdb9c55c122a8dc551db6eeecac35ed
VillarrealOctavio <octavillarreal2016@gmail.com>
2022-02-15 10:12:00 -0300

    Modificando modal

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,16 @@
+// Minuto 46 del video.
 // Contenedores de productos para incluir estructura HTML en su interior
 const stockDeMates = document.querySelector(".todosMates")
 const stockDeBolsos = document.querySelector(".todosBolsos")
 const stockDeTermos = document.querySelector(".todosTermos")
 const imgAndinista = document.querySelector(".andinista")
-const productosEnCarrito = document.querySelector(".composicionCarrito")
+const carritoModal = document.querySelector(".modalCarrito")
 
 // Botones para incluir un "evento".
 const btnBolso = document.querySelector("#botonBolsos")
 const btnMate = document.querySelector("#botonMates")
 const btnTermo = document.querySelector("#botonTermos")
 const filterMate = document.querySelector("#filtrarMate")
-const showCarrito = document.querySelector("#mostrarCarrito")
 
 // Arrays
 let enElCarrito = [];
@@ -20,7 +20,7 @@ btnMate.addEventListener(`click`, ()=>{
     showProduct(stockMates)
 })
 
-filterMate.addEventListener(`change`,()=> {
+filterMate.addEventListener(`change`,()=> { //Preguntar este ejercicio al profe
     console.log(filterMate.value)
     if (filterMate.value=="todos") {
         showProduct(stockMates)
@@ -28,12 +28,6 @@ filterMate.addEventListener(`change`,()=> {
         showProduct(stockMates.filter(el=>el.tipo.includes(filterMate.value)))
     }
 })
-
-showCarrito.addEventListener(`click`, ()=>{
-    agregarAlCarritoDeCompras()
-})   //Ayer me quedé trabado en este ejercicio. Minuto 43 del video.
-
-
 
 
 // Funciones
@@ -81,6 +75,8 @@ function showProduct (array) {
         })
     });
 }
+// ------------------------------------------------------------
+
 
 function agregarAlCarritoDeCompras(id) {
  let agregarElProducto = stockMates.find(el=>el.id==id);
@@ -89,35 +85,19 @@ function agregarAlCarritoDeCompras(id) {
 
  let div = document.createElement("div")
  div.innerHTML= `
-                <div class="modal" tabindex="-1">
-                <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h5 class="modal-title">Viejas al Hilo</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <div class="modal-body">
-                    <span>Mate: ${agregarElProducto.tipo}</span> <span>Precio: $${agregarElProducto.precio}</span> 
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Precio total: </button>
-                    </div>
-                </div>
-                </div>
-                </div>
+            <p>Producto: ${agregarElProducto.tipo}</p>
+            <p>Precio: $${agregarElProducto.precio}</p>
+            <p>Cantidad: ${agregarElProducto.cantidad}</p>
  `
- productosEnCarrito.appendChild(div)
+ carritoModal.appendChild(div)
 };
 
-
+// ------------------------------------------------------------
 function actualizarCarrito (){
 
 };
 
-
+// -----------------------------------------------------------
 
 function recuperar (){
 
