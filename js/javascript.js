13ab06af2795605c4315a7a5610fe4604667f58d
VillarrealOctavio <octavillarreal2016@gmail.com>
2022-02-16 20:05:42 -0300

    Agregando Storage y JSON

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,10 @@
-// Minuto 46 del video.
 // Contenedores de productos para incluir estructura HTML en su interior
 const stockDeMates = document.querySelector(".todosMates")
 const stockDeBolsos = document.querySelector(".todosBolsos")
 const stockDeTermos = document.querySelector(".todosTermos")
 const imgAndinista = document.querySelector(".andinista")
 const carritoModal = document.querySelector(".modalCarrito")
+const totalPrice = document.querySelector("#precioTotal")
 
 // Botones para incluir un "evento".
 const btnBolso = document.querySelector("#botonBolsos")
@@ -25,7 +25,7 @@ filterMate.addEventListener(`change`,()=> { //Preguntar este ejercicio al profe
     if (filterMate.value=="todos") {
         showProduct(stockMates)
     }else{
-        showProduct(stockMates.filter(el=>el.tipo.includes(filterMate.value)))
+        showProduct(stockMates.filter(el=> el.color==filterMate.value))
     }
 })
 
@@ -33,28 +33,8 @@ filterMate.addEventListener(`change`,()=> { //Preguntar este ejercicio al profe
 // Funciones
 function showProduct (array) {
     imgAndinista.innerHTML=""; 
-    let filtrar = document.createElement("Select")
-    filtrar.id=`filtrarMate`
-    filtrar.innerHTML=`          
-                <option value="todos">Todos</option>
-                <option value="azul">Azul</option>
-                <option value="blanco">Blanco</option>
-                <option value="caramelo">Caramelo</option>
-                <option value="carmin">Carmin</option>
-                <option value="celeste">Celeste</option>
-                <option value="fucsia">Fucsia</option>
-                <option value="lila">Lila</option>
-                <option value="rosa">Rosa</option>
-                <option value="terracota">Terracota</option>
-                <option value="turqueza">Turqueza</option>
-                <option value="verde">Verde</option>
-                <option value="violeta">Violeta</option>
-                <option value="marrón">Marrón</option>
-                <option value="naranja">Naranja</option>
-                <option value="negro">Negro</option>
-                <option value="rojo">Rojo</option>
-    `   
-        stockDeMates.prepend(filtrar)
+    stockDeMates.innerHTML="";
+
     array.forEach(el =>{
         let div = document.createElement("div");
         div.innerHTML += `
@@ -79,27 +59,40 @@ function showProduct (array) {
 
 
 function agregarAlCarritoDeCompras(id) {
- let agregarElProducto = stockMates.find(el=>el.id==id);
- console.log(agregarElProducto);
- enElCarrito.push(agregarElProducto)
-
- let div = document.createElement("div")
- div.innerHTML= `
-            <p>Producto: ${agregarElProducto.tipo}</p>
-            <p>Precio: $${agregarElProducto.precio}</p>
-            <p>Cantidad: ${agregarElProducto.cantidad}</p>
- `
- carritoModal.appendChild(div)
+ let repeated = enElCarrito.find(item=>item.id==id)
+ /*if(repeated){
+     repeated.cantidad=repeated.cantidad + 1;
+ }else{*/
+    let agregarElProductoAlCarrito = stockMates.find(el=>el.id==id);
+    console.log(agregarElProductoAlCarrito);
+    enElCarrito.push(agregarElProductoAlCarrito)
+    actualizarCarrito();
+       carritoModal.innerHTML += `
+               <p>Producto: ${agregarElProductoAlCarrito.tipo}</p>
+               <p>Precio: $${agregarElProductoAlCarrito.precio}</p>
+               <p id="cantidad${agregarElProductoAlCarrito.id}">Cantidad: ${agregarElProductoAlCarrito.cantidad}</p>
+               <button id="btnEliminar${agregarElProductoAlCarrito.id}">Eliminar Producto</button>
+    `
+       const btnEliminar = document.querySelector(`#btnEliminar${agregarElProductoAlCarrito.id}`)
+       btnEliminar.addEventListener(`click`,()=>{
+           console.log(btnEliminar)})
+        //    console.log(btnEliminar.parentElement)
+   
+    //    enElCarrito = enElCarrito.filter(el => el.id != agregarElProductoAlCarrito.id)
+ //}
+//  localStorage.setItem=(`carritoDeCompra`, JSON.stringify(enElCarrito))
 };
 
 // ------------------------------------------------------------
 function actualizarCarrito (){
-
+    totalPrice.innerText= enElCarrito.reduce((acc, producto)=>acc+(producto.cantidad*producto.precio), 0);
 };
 
 // -----------------------------------------------------------
 
 function recuperar (){
-
+    // let recuperar = JSON.parse(`carritoDeCompras`)
 };
-
+let mail = document.querySelector(".correo");
+localStorage.setItem("correoElectronico", JSON.stringify(mail))
+let obtener = JSON.parse(localStorage.getItem("correoElectronico"))
